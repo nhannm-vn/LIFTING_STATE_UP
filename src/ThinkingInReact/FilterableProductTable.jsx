01883ec1f6fbb13dfda00dd5a8c82731c7af3f74
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-05 06:28:30 +0700

    update 5-12-2024

--- src/ThinkingInReact/FilterableProductTable.jsx
+++ src/ThinkingInReact/FilterableProductTable.jsx
@@ -56,7 +56,7 @@ const fetchApi = () => {
 export class FilterableProductTable extends Component {
   constructor(props) {
     super(props)
-
+    //_Các state ban đầu đều chưa có giá trị gì cảs
     this.state = {
       productList: [],
       searchText: '',
@@ -64,13 +64,41 @@ export class FilterableProductTable extends Component {
     }
   }
 
+  //_sau khi mount xong thì call api lấy data
+  async componentDidMount() {
+    const data = await fetchApi()
+    this.setState((prevState) => ({
+      ...prevState,
+      productList: data
+    }))
+  }
+
+  //_method nãy sẽ nhận vào event và sẽ chạy và setSate lại khi mà bên component
+  //SearchBar các ô input check-box thay đổi
+  //_Nen la arow function
+  handleChange = (event) => {
+    const name = event.target.name
+    if (name === 'searchText') {
+      this.setState((prevState) => ({
+        ...prevState,
+        searchText: event.target.value
+      }))
+    } else {
+      this.setState((prevState) => ({
+        ...prevState,
+        searchText: event.target.checked
+      }))
+    }
+  }
+
   render() {
+    const { searchText, inStock, productList } = this.state
     return (
       <div className='FilterableProductTable'>
         {/* SearchBar (blue) */}
-        <SearchBar />
+        <SearchBar searchText={searchText} inStock={inStock} handleChange={this.handleChange} />
         {/*  ProductTable (green) */}
-        <ProductTable productList={productListMock} />
+        <ProductTable searchText={searchText} inStock={inStock} productList={productList} />
       </div>
     )
   }
--- src/ThinkingInReact/SearchBar.jsx
+++ src/ThinkingInReact/SearchBar.jsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 
 export class SearchBar extends Component {
   render() {
+    const { searchText, inStock, handleChange } = this.props
     return (
       <form>
-        <input type='text' placeholder='Search...' />
+        <input type='text' name='searchText' value={searchText} placeholder='Search...' onChange={handleChange} />
         <div>
-          <input type='checkbox' />
+          <input type='checkbox' value={inStock} name='inStock' onChange={handleChange} />
           <label>Only show products in stock</label>
         </div>
       </form>
