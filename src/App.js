3bf9517dbb40f0f56cd65f8eb699dd5c756fffcc
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-05 06:52:27 +0700

    update 5-12-2024

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg'
 import './App.css'
 import FilterableProductTable from './ThinkingInReact/FilterableProductTable'
-// import Calculator from './Calculator/Calculator'
+import Calculator from './Calculator/Calculator'
 
 function App() {
   return (
     <div className='App'>
-      {/* <Calculator /> */}
+      <Calculator />
       <FilterableProductTable />
     </div>
   )
