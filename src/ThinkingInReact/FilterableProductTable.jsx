e36e20b7ce9a9311c4dcf37a9f6a5c4581446ba8
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-05 06:07:51 +0700

    update 5-12-2024

--- src/ThinkingInReact/FilterableProductTable.jsx
+++ src/ThinkingInReact/FilterableProductTable.jsx
@@ -8,15 +8,62 @@ import SearchBar from './SearchBar'
 import ProductTable from './ProductTable'
 
 const productListMock = [
-  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+  {
+    category: 'Sporting Goods',
+    price: '$49.99', //
+    stocked: true,
+    name: 'Football'
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99', //
+    stocked: true,
+    name: 'Baseball'
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99', //
+    stocked: false,
+    name: 'Basketball'
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99', //
+    stocked: true,
+    name: 'iPod Touch'
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99', //
+    stocked: false,
+    name: 'iPhone 5'
+  },
+  {
+    category: 'Electronics',
+    price: '$199.99', //
+    stocked: true,
+    name: 'Nexus 7'
+  }
 ]
 
+//_Gỉa lập api
+const fetchApi = () => {
+  return new Promise((resolve, reject) => {
+    resolve(productListMock)
+  })
+}
+
 export class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      productList: [],
+      searchText: '',
+      inStock: false
+    }
+  }
+
   render() {
     return (
       <div className='FilterableProductTable'>
