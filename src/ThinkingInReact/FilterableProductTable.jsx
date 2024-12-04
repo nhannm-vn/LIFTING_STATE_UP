f7fe9140a0752cea3fb24e40f6ccf7224e61b51c
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-04 19:05:20 +0700

    update 12-4-2024

--- src/ThinkingInReact/FilterableProductTable.jsx
+++ src/ThinkingInReact/FilterableProductTable.jsx
@@ -1,10 +1,40 @@
 //Chứa toàn bộ ví dụ chúng ta
 
+//import css
+import './FilterableProductTable.css'
+
 import React, { Component } from 'react'
 
 export class FilterableProductTable extends Component {
   render() {
-    return <div>FilterableProductTable</div>
+    return (
+      <div className='FilterableProductTable'>
+        <form>
+          <input type='text' placeholder='Search...' />
+          <div>
+            <input type='checkbox' />
+            <label>Only show products in stock</label>
+          </div>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th colSpan={2}>Sporting Goods</th>
+            </tr>
+            <tr>
+              <td>Football</td>
+              <td>$49.99</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
